93e3769b0efd509cc4b158e4718205ce1328c246
HumamShwaikh <hshwa039@uottawa.ca>
2020-04-13 20:27:50 -0400

    HumamAndIvorsSickHomeAutoNetwork

--- Home_IoT_PIO/src/main.cpp
+++ Home_IoT_PIO/src/main.cpp
@@ -8,8 +8,8 @@
 //************************************************************
 #include <painlessMesh.h>
 
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PREFIX     "HumamAndIvorsSickHomeAutoNetwork"
+#define   MESH_PASSWORD   "passypasspass"
 #define   MESH_PORT       5555
 
 Scheduler userScheduler; // to control your personal task
