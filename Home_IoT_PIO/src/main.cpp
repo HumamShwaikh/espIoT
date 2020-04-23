1b4a1c9d5d3b9a754a30346542881bcdcf7f2ea7
Ivor Benderavage <ivor.benderavage@gmail.com>
2020-04-23 00:54:52 -0400

    Added LogClient example code into main.cpp

--- Home_IoT_PIO/src/main.cpp
+++ Home_IoT_PIO/src/main.cpp
@@ -1,80 +1,96 @@
 //************************************************************
-// this is a simple example that uses the painlessMesh library
-//
-// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
-// 2. prints anything it receives to Serial.print
-//
-//
+// this is a simple example that uses the painlessMesh library to 
+// setup a node that logs to a central logging node
+// The logServer example shows how to configure the central logging nodes
 //************************************************************
-#include <painlessMesh.h>
+#include "painlessMesh.h"
 
-#define   LED_PIN         2  // Builtin LED - Pin "TX2" or "GPIO5"
-
-#define   MESH_PREFIX     "AutoNetwork"
+#define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "passypasspass"
 #define   MESH_PORT       5555
 
-Scheduler userScheduler; // to control your personal task
+Scheduler     userScheduler; // to control your personal task
 painlessMesh  mesh;
 
-// User stub
-void sendMessage() ; // Prototype so PlatformIO doesn't complain
-void toggleLight();
+// Prototype
+void receivedCallback( uint32_t from, String &msg );
 
-int ledState = LOW;
+size_t logServerId = 0;
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+// Send message containing photoresistor value to the logServer every 10 seconds 
+Task myLoggingTask(10000, TASK_FOREVER, []() {
+#if ARDUINOJSON_VERSION_MAJOR==6
+        DynamicJsonDocument jsonBuffer(1024);
+        JsonObject msg = jsonBuffer.to<JsonObject>();
+#else
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& msg = jsonBuffer.createObject();
+#endif
+    msg["topic"] = "photoresistor";
+    msg["value"] = analogRead(A0);
 
-void sendMessage() {
-  String msg = "Hello from node Ass-";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
-}
+    String str;
+#if ARDUINOJSON_VERSION_MAJOR==6
+    serializeJson(msg, str);
+#else
+    msg.printTo(str);
+#endif
+    if (logServerId == 0) // If we don't know the logServer yet
+        mesh.sendBroadcast(str);
+    else
+        mesh.sendSingle(logServerId, str);
 
-// Needed for painless library
-void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-  toggleLight();
-}
-
-void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
-
-void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
-}
-
-void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-}
-
-void toggleLight() {
-    ledState == LOW ? ledState=HIGH:ledState=LOW;
-    digitalWrite(LED_PIN, ledState);
-}
+    // log to serial
+#if ARDUINOJSON_VERSION_MAJOR==6
+    serializeJson(msg, Serial);
+#else
+    msg.printTo(Serial);
+#endif
+    Serial.printf("\n");
+});
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, ledState);
+  pinMode(A0, INPUT);
+    
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 6 );
   mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  userScheduler.addTask( taskSendMessage );
-  taskSendMessage.enable();
+  // Add the task to the your scheduler
+  userScheduler.addTask(myLoggingTask);
+  myLoggingTask.enable();
 }
 
 void loop() {
   // it will run the user scheduler as well
   mesh.update();
+}
+
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
+
+  // Saving logServer
+#if ARDUINOJSON_VERSION_MAJOR==6
+  DynamicJsonDocument jsonBuffer(1024 + msg.length());
+  DeserializationError error = deserializeJson(jsonBuffer, msg);
+  if (error) {
+    Serial.printf("DeserializationError\n");
+    return;
+  }
+  JsonObject root = jsonBuffer.as<JsonObject>();
+#else
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(msg);
+#endif
+  if (root.containsKey("topic")) {
+      if (String("logServer").equals(root["topic"].as<String>())) {
+          // check for on: true or false
+          logServerId = root["nodeId"];
+          Serial.printf("logServer detected!!!\n");
+      }
+      Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
+  }
 }
