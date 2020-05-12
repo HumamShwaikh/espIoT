f348244863ae08d339712068e11f5d977b28ddef
Ivor <ivor.benderavage@gmail.com>
2020-05-11 21:54:00 -0400

    Fixed JSON interpret logic

--- Home_IoT_PIO/rpi_logger/main.cpp
+++ Home_IoT_PIO/rpi_logger/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <sstream>
 #include <algorithm>
 #include "serialMonitor.hpp"
 #include "sample.cpp"  // Don't know why we need this...?  But the linker complains if we remove it.
@@ -14,6 +16,7 @@ using namespace std;
 
 //Prototype
 string getSubstringBetweenDelimiters(string inputString, char startDelimiter, char endDelimiter);
+std::string escape_json(const std::string &s);
 
 int main() {
 
@@ -23,21 +26,29 @@ int main() {
 
     while(true) {
 
-        string data = sm.getData();
+        std::string data = sm.getData();
 
         if(data != ""){
 
             data = getSubstringBetweenDelimiters(data, '{', '}');
 
-            json jsonData = data;
+            cout << data << "Heuh" << endl;
 
-            Sample sample(jsonData["topic"], 69.696, "unit");
+            json jsonData = json::parse(data);
 
-            cout << sample << endl << LINE_BREAK << endl;
+            cout << std::setw(8) << jsonData << endl;
+
+            cout << jsonData["topic"] << endl;
+
+            //Sample sample(jsonData["topic"], 69.696, "unit");
+
+            //cout << sample << endl << LINE_BREAK << endl;
+
+            cout << LINE_BREAK << endl;
 
         }
 
-        usleep(DELAY);
+        usleep(DELAY*2);
 
     }
 
@@ -70,4 +81,27 @@ string getSubstringBetweenDelimiters(string inputString, char startDelimiter, ch
 
     return inputString.substr(first, last - first + 1);
 
+}
+
+std::string escape_json(const std::string &s) {
+    std::ostringstream o;
+    for (auto c = s.cbegin(); c != s.cend(); c++) {
+        switch (*c) {
+        case '"': o << "\\\""; break;
+        case '\\': o << "\\\\"; break;
+        case '\b': o << "\\b"; break;
+        case '\f': o << "\\f"; break;
+        case '\n': o << "\\n"; break;
+        case '\r': o << "\\r"; break;
+        case '\t': o << "\\t"; break;
+        default:
+            if ('\x00' <= *c && *c <= '\x1f') {
+                o << "\\u"
+                  << std::hex << std::setw(4) << std::setfill('0') << (int)*c;
+            } else {
+                o << *c;
+            }
+        }
+    }
+    return o.str();
 }
--- Home_IoT_PIO/rpi_logger/serialMonitor.cpp
+++ Home_IoT_PIO/rpi_logger/serialMonitor.cpp
@@ -54,7 +54,7 @@ using namespace std;
 
             }
             else {
-                receivedData = "\t---Nothing available.---";
+                receivedData = "";
             }
 
             return(receivedData);
